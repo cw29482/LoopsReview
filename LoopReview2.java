5bd2c2eacfc2733c35a37dc6cc3585cefbd2c8df
cw29482 <cw29482@email.vccs.edu>
2015-04-01 15:10:29 -0400

    Part 2

---
+++ LoopReview2.java
@@ -0,0 +1,16 @@
+
+public class LoopReview2 {
+	public static void main(String [] args) {
+		int evenNum, count=1;
+		
+		do {
+			evenNum = 2*count;
+			count ++;
+			System.out.println(evenNum);
+			
+		}while (count < 200);
+		
+	}
+	
+
+}
