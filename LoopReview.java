5dc31b3d234b9be4036376ad17cbd2ddcd021cf2
cw29482 <cw29482@email.vccs.edu>
2015-04-01 14:47:59 -0400

    Part 1

---
+++ LoopReview.java
@@ -0,0 +1,13 @@
+
+public class LoopReview {
+	public static void main(String [] args) {
+		int oddNum, count;
+		
+		for(count = 0; count<100; count++) {
+			oddNum = 1 + 2*count;
+			System.out.println(oddNum);
+		}
+		
+	}
+
+}
